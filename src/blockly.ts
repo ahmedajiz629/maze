6d2ceb0c57f27b02c831f477c8d48f7036a476f3
ahmed <ahmed>
2025-10-12 13:50:57 +0100

    blockly

--- src/blockly.ts
+++ src/blockly.ts
@@ -4,6 +4,103 @@ import { ActionName, Levels } from "./level";
 const functions: ActionName[] = ['step', 'left', 'right', 'safe', 'notDone', 'checkRight', 'checkLeft'] as const;
 
 export const levels: Levels = {
+  1: () => ({
+    MAP: [
+      "#######",
+      "#######",
+      "#######",
+      "#######",
+      "##S.E##",
+      "#######",
+      "#######",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
+  2: () => ({
+    MAP: [
+      "########",
+      "########",
+      "########",
+      "###.E###",
+      "##S.####",
+      "########",
+      "########",
+      "########",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
+  3: () => ({
+    MAP: [
+      "########",
+      "########",
+      "########",
+      "########",
+      "#S....E#",
+      "########",
+      "########",
+      "########",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
+  4: () => ({
+    MAP: [
+      "#######.",
+      "######..",
+      "#####E.#",
+      "####..##",
+      "###..###",
+      "##..####",
+      "#S.#####",
+      "........",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
+  5: () => ({
+    MAP: [
+      "########",
+      "#####E##",
+      "#####.##",
+      "#####.##",
+      "#####.##",
+      "#####.##",
+      "###S..##",
+      "########",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
+  6: () => ({
+    MAP: [
+      "########",
+      "########",
+      "#......#",
+      "#.###..#",
+      "#...E..#",
+      "#####..#",
+      "#S....##",
+      "########",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
+  7: () => ({
+    MAP: [
+      "########",
+      "#####..#",
+      "#S....##",
+      "#####..#",
+      "###E#..#",
+      "##.##..#",
+      "##......#",
+      "########",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
   8: () => ({
     MAP: [
       "#######",
@@ -13,6 +110,20 @@ export const levels: Levels = {
       "###.#.#",
       "#S..#E#",
       "#######",
+    ].map(x=>[...x].reverse().join('')),
+    ROTATION: Math.PI,
+    functions
+  }),
+  9: () => ({
+    MAP: [
+      "########",
+      "#.....##",
+      "##.###.#",
+      "E......#",
+      "#.#.#.##",
+      "....#.##",
+      "#S.##.##",
+      "########",
     ].map(x=>[...x].reverse().join('')),
     ROTATION: Math.PI,
     functions
